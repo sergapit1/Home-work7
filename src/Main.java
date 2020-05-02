e94b2636422900e825df047c96ddbc421e9b7940
sergapit1 <serg.apit0@gmail.com>
2020-05-02 15:49:20 +0300

    3

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,25 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
 
         Generator r = new Generator();
-        Person[] person = new Person[10];
+        Person[] person = new Person[100];
         for (int i = 0; i < person.length; i++) {
             person[i] = new Person(r.getName(), r.getAge(), r.getDate());
-            //  System.out.println(person[i]);
+           // System.out.println(person[i]);
+
 
         }
-        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("person3.txt"));
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("person4.bin"));
+
 
         oos.writeObject(person);
         oos.close();
--- src/MainRead.java
+++ src/MainRead.java
@@ -1,21 +1,33 @@
+import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainRead {
 
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
+     ObjectInputStream ois = new ObjectInputStream(new FileInputStream("person4.bin"));
 
-        ObjectInputStream ois =new ObjectInputStream(new FileInputStream("person.txt"));
-        Object obj=ois.readObject();
-       // Person=(TestClass[])obj;
-        ois.close();
+        ArrayList<Person> person1 = new ArrayList<>();
 
 
-        System.out.println(ois);
+           try {
+
+        person1.add((Person)ois.readObject());
+               ois.close();
+
+            } catch (ClassCastException e) {
+
+
+       }
+
+        System.out.println(person1);
+
     }
 }
-
 
