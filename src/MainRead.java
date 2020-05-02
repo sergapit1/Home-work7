c927322cbfdd1c949178d64a78d520ebc763c049
sergapit1 <serg.apit0@gmail.com>
2020-05-03 02:18:37 +0300

    7

--- src/MainRead.java
+++ src/MainRead.java
@@ -20,12 +20,26 @@ public class MainRead {
 
         person1.sort((o1, o2) -> o2.getNick().charAt(0) - o1.getNick().charAt(0));
 
-
         System.out.println("After sort by nick " + person1);
-        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("sortperson.txt"));
-        oos.writeByte(person1);
 
-        //  list.sort(String::compareToIgnoreCase);
+
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("sortperson.txt"));
+        String space = " ";
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (i < person1.size() - 1) {
+            sb.append(person1.get(i));
+            sb.append(space);
+            i++;
+        }
+        sb.append(person1.get(i));
+
+        String person2 = sb.toString();
+        System.out.println("String : " + person2);
+        // oos.writeUTF(person2);
+        oos.writeBytes(person2);
+        oos.close();
+
     }
 }
 
